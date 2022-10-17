4b3c9363232470ff2f52a4dc3ca839d91967c6ac
Rich <rufhinator@hotmail.com>
2022-10-17 12:22:23 -0400

    -2c Complete

--- src/main/java/com/example/comp1011s1st200287453/CarSaleApplication.java
+++ src/main/java/com/example/comp1011s1st200287453/CarSaleApplication.java
@@ -11,7 +11,7 @@ public class CarSaleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CarSaleApplication.class.getResource("application-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Car Sales Application");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/comp1011s1st200287453/CarSaleController.java
+++ src/main/java/com/example/comp1011s1st200287453/CarSaleController.java
@@ -1,14 +1,34 @@
 package com.example.comp1011s1st200287453;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 
-public class CarSaleController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CarSaleController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<?, ?> carID;
+
+    @FXML
+    private TableColumn<?, ?> dateSold;
+
+    @FXML
+    private TableColumn<?, ?> make;
+
+    @FXML
+    private TableColumn<?, ?> model;
+
+    @FXML
+    private TableColumn<?, ?> modelYear;
+
+    @FXML
+    private TableColumn<?, ?> price;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
